5db171b6108153e76bda602f6828f0896d862de8
georgelipceanu <george.lipceanu@gmail.com>
2024-10-03 14:04:17 +0100

    added private ip to index.html in instance

--- devops_ass1.py
+++ devops_ass1.py
@@ -1,22 +1,33 @@
 import boto3
 from datetime import datetime
 import json
+import subprocess
 
+# ADJUSTABLE VARIABLES
 keypair = 'rfstudentkey'
 sg_ids = ['sg-015185af0d0cd3ff9']
-image_id = 'ami-0ebfd941bbafe70c6'
+instance_image_id = 'ami-0ebfd941bbafe70c6'
+userdata = """#!/bin/bash
+        yum install httpd -y
+        systemctl enable httpd
+        systemctl start httpd
+        echo '<html>' > index.html
+        echo 'Private IP address: ' >> index.html
+        TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+        curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/local-ipv4 >> index.html
+        echo '<br>Instance ID: ' >> index.html
+        cp index.html /var/www/html/index.html"""
+
+IMAGE_URL = 'http://devops.witdemo.net/logo.jpg'
 
 ec2 = boto3.resource('ec2')
 new_instances = ec2.create_instances(
-    ImageId=image_id,
+    ImageId=instance_image_id,
     MinCount=1,
     MaxCount=1,
     SecurityGroupIds=sg_ids,
     KeyName=keypair,
-    UserData="""#!/bin/bash
-        yum install httpd -y
-        systemctl enable httpd
-        systemctl start httpd""",
+    UserData=userdata,
     InstanceType='t2.nano',
     TagSpecifications=[
         {
@@ -32,41 +43,40 @@ print(instance_id)
 new_instances[0].wait_until_running()
 print("Instance Running.")
 
-s3 = boto3.resource("s3")
-s3_client = boto3.client("s3")
+# s3 = boto3.resource("s3")
+# s3_client = boto3.client("s3")
 
-bucket_name = f"projectx-bucket1-{datetime.now().strftime('%Y-%m-%d-%s')}"
-try:
-    response = s3.create_bucket(Bucket=bucket_name)
-    print (response)
+# bucket_name = f"{datetime.now().strftime('%Y-%m-%d-%s')}-glipceanu"
+# try:
+#     response = s3.create_bucket(Bucket=bucket_name)
+#     print (response)
 
-    s3_client.delete_public_access_block(Bucket=bucket_name)
+#     s3_client.delete_public_access_block(Bucket=bucket_name)
 
-    website_configuration = {
-        'ErrorDocument': {'Key': 'error.html'},
-        'IndexDocument': {'Suffix': 'index.html'},
-    }
-    bucket_website = s3.BucketWebsite(bucket_name)
-    response = bucket_website.put(WebsiteConfiguration=website_configuration)
-    print(response)
+#     website_configuration = {
+#         'ErrorDocument': {'Key': 'error.html'},
+#         'IndexDocument': {'Suffix': 'index.html'},
+#     }
+#     bucket_website = s3.BucketWebsite(bucket_name)
+#     response = bucket_website.put(WebsiteConfiguration=website_configuration)
+#     print(response)
 
-    bucket_policy = {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Sid": "PublicReadGetObject",
-                "Effect": "Allow",
-                "Principal": "*",
-                "Action": "s3:GetObject",
-                "Resource": f"arn:aws:s3:::{bucket_name}/*"
-            }
-        ]
-    }
+#     bucket_policy = {
+#         "Version": "2012-10-17",
+#         "Statement": [
+#             {
+#                 "Sid": "PublicReadGetObject",
+#                 "Effect": "Allow",
+#                 "Principal": "*",
+#                 "Action": "s3:GetObject",
+#                 "Resource": f"arn:aws:s3:::{bucket_name}/*"
+#             }
+#         ]
+#     }
 
-    # Convert the policy to JSON and apply it
-    bucket_policy_json = json.dumps(bucket_policy)
-    s3_client.put_bucket_policy(Bucket=bucket_name, Policy=bucket_policy_json)
+#     bucket_policy_json = json.dumps(bucket_policy) # convert the policy to JSON and apply it
+#     s3_client.put_bucket_policy(Bucket=bucket_name, Policy=bucket_policy_json)
 
-    print("Upload an index.html file to test it works!")
-except Exception as error:
-    print (error)
+#     print("Upload an index.html file to test it works!")
+# except Exception as error:
+#     print (error)
