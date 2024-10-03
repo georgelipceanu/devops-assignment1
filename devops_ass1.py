57efef22524fdfd53f0570d4df543d74641c83ee
georgelipceanu <george.lipceanu@gmail.com>
2024-10-03 14:48:56 +0100

    added instance id to user data

--- devops_ass1.py
+++ devops_ass1.py
@@ -16,6 +16,9 @@ userdata = """#!/bin/bash
         TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
         curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/local-ipv4 >> index.html
         echo '<br>Instance ID: ' >> index.html
+        curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id >> index.html
+        echo '<br>Availabilty Zone: ' >> index.html
+        
         cp index.html /var/www/html/index.html"""
 
 IMAGE_URL = 'http://devops.witdemo.net/logo.jpg'
