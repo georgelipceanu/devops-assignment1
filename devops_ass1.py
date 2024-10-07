787d0d956b5a0c3acfdaaa8a7ba47941c2a4692d
georgelipceanu <george.lipceanu@gmail.com>
2024-10-07 23:13:03 +0100

    added image handling

--- devops_ass1.py
+++ devops_ass1.py
@@ -26,64 +26,68 @@ userdata = f"""#!/bin/bash
         echo '<br> {addtional_text}' >> index.html
         cp index.html /var/www/html/index.html"""
 IMAGE_URL = 'http://devops.witdemo.net/logo.jpg'
+print("Downloading Logo...")
+subprocess.run(["curl", "-o", 'logo.jpg', IMAGE_URL])
 
-ec2 = boto3.resource('ec2')
-new_instances = ec2.create_instances(
-    ImageId=instance_image_id,
-    MinCount=1,
-    MaxCount=1,
-    SecurityGroupIds=sg_ids,
-    KeyName=keypair,
-    UserData=userdata,
-    InstanceType='t2.nano',
-    TagSpecifications=[
-        {
-            'ResourceType': 'instance',
-            'Tags': [
-                {'Key': 'Name', 'Value': 'MyWebServer'},
-            ]
-        }
-    ])
+# ec2 = boto3.resource('ec2')
+# new_instances = ec2.create_instances(
+#     ImageId=instance_image_id,
+#     MinCount=1,
+#     MaxCount=1,
+#     SecurityGroupIds=sg_ids,
+#     KeyName=keypair,
+#     UserData=userdata,
+#     InstanceType='t2.nano',
+#     TagSpecifications=[
+#         {
+#             'ResourceType': 'instance',
+#             'Tags': [
+#                 {'Key': 'Name', 'Value': 'MyWebServer'},
+#             ]
+#         }
+#     ])
 
-instance_id = new_instances[0].id
-print(instance_id)
-new_instances[0].wait_until_running()
-print("Instance Running.")
+# instance_id = new_instances[0].id
+# print(instance_id)
+# new_instances[0].wait_until_running()
+# print("Instance Running.")
 
-# s3 = boto3.resource("s3")
-# s3_client = boto3.client("s3")
+s3 = boto3.resource("s3")
+s3_client = boto3.client("s3")
 
-# bucket_name = f"{datetime.now().strftime('%Y-%m-%d-%s')}-glipceanu"
-# try:
-#     response = s3.create_bucket(Bucket=bucket_name)
-#     print (response)
+bucket_name = f"{datetime.now().strftime('%Y-%m-%d-%s')}-glipceanu"
+try:
+    response = s3.create_bucket(Bucket=bucket_name)
+    print (response)
 
-#     s3_client.delete_public_access_block(Bucket=bucket_name)
+    s3_client.delete_public_access_block(Bucket=bucket_name)
 
-#     website_configuration = {
-#         'ErrorDocument': {'Key': 'error.html'},
-#         'IndexDocument': {'Suffix': 'index.html'},
-#     }
-#     bucket_website = s3.BucketWebsite(bucket_name)
-#     response = bucket_website.put(WebsiteConfiguration=website_configuration)
-#     print(response)
+    website_configuration = {
+        'ErrorDocument': {'Key': 'error.html'},
+        'IndexDocument': {'Suffix': 'index.html'},
+    }
+    bucket_website = s3.BucketWebsite(bucket_name)
+    response = bucket_website.put(WebsiteConfiguration=website_configuration)
+    print(response)
 
-#     bucket_policy = {
-#         "Version": "2012-10-17",
-#         "Statement": [
-#             {
-#                 "Sid": "PublicReadGetObject",
-#                 "Effect": "Allow",
-#                 "Principal": "*",
-#                 "Action": "s3:GetObject",
-#                 "Resource": f"arn:aws:s3:::{bucket_name}/*"
-#             }
-#         ]
-#     }
+    bucket_policy = {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "PublicReadGetObject",
+                "Effect": "Allow",
+                "Principal": "*",
+                "Action": "s3:GetObject",
+                "Resource": f"arn:aws:s3:::{bucket_name}/*"
+            }
+        ]
+    }
 
-#     bucket_policy_json = json.dumps(bucket_policy) # convert the policy to JSON and apply it
-#     s3_client.put_bucket_policy(Bucket=bucket_name, Policy=bucket_policy_json)
+    s3.Bucket(bucket_name).upload_file('logo.jpg', 'logo.jpg', ExtraArgs={'ContentType': 'image/jpeg'})
 
-#     print("Upload an index.html file to test it works!")
-# except Exception as error:
-#     print (error)
+    bucket_policy_json = json.dumps(bucket_policy) # convert the policy to JSON and apply it
+    s3_client.put_bucket_policy(Bucket=bucket_name, Policy=bucket_policy_json)
+
+    print("check it")
+except Exception as error:
+    print (error)
