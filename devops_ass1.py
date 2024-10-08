a6148f366485335886872e7cd6265738fb324b3a
georgelipceanu <george.lipceanu@gmail.com>
2024-10-08 22:56:17 +0100

    added monitoring setup and logging to file

--- devops_ass1.py
+++ devops_ass1.py
@@ -1,9 +1,15 @@
+import logging.config
 import boto3
 from datetime import datetime
 import json
 import subprocess
+import logging
 
+# ERROR LOGGING SETUP
 subprocess.run("clear")
+logging.basicConfig(filename='logs.txt', level=logging.ERROR, 
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
 # ADJUSTABLE VARIABLES
 addtional_text = "This is additional text! :)"
 keypair = 'rfstudentkey'
@@ -26,68 +32,96 @@ userdata = f"""#!/bin/bash
         echo '<br> {addtional_text}' >> index.html
         cp index.html /var/www/html/index.html"""
 IMAGE_URL = 'http://devops.witdemo.net/logo.jpg'
-print("Downloading Logo...")
-subprocess.run(["curl", "-o", 'logo.jpg', IMAGE_URL])
 
-# ec2 = boto3.resource('ec2')
-# new_instances = ec2.create_instances(
-#     ImageId=instance_image_id,
-#     MinCount=1,
-#     MaxCount=1,
-#     SecurityGroupIds=sg_ids,
-#     KeyName=keypair,
-#     UserData=userdata,
-#     InstanceType='t2.nano',
-#     TagSpecifications=[
-#         {
-#             'ResourceType': 'instance',
-#             'Tags': [
-#                 {'Key': 'Name', 'Value': 'MyWebServer'},
-#             ]
-#         }
-#     ])
+# KEYPAIR SETUP
+subprocess.run(["sudo", "chmod", "700", f"{keypair}.pem"], check=True)
 
-# instance_id = new_instances[0].id
-# print(instance_id)
-# new_instances[0].wait_until_running()
-# print("Instance Running.")
+# IMAGE SETUP
+# print("Downloading Logo...")
+# try:
+#     subprocess.run(["curl", "-o", 'logo.jpg', IMAGE_URL], check=True)
+#     print("Download Complete!")
+# except Exception as error:
+#     print("Failed to download Logo.")
+#     logging.error(error)  
 
-s3 = boto3.resource("s3")
-s3_client = boto3.client("s3")
+# SPINNING UP INSTANCE
+ec2 = boto3.resource('ec2')
+ec2_client = boto3.client('ec2')
+new_instances = ec2.create_instances(
+    ImageId=instance_image_id,
+    MinCount=1,
+    MaxCount=1,
+    SecurityGroupIds=sg_ids,
+    KeyName=keypair,
+    UserData=userdata,
+    InstanceType='t2.nano',
+    TagSpecifications=[
+        {
+            'ResourceType': 'instance',
+            'Tags': [
+                {'Key': 'Name', 'Value': 'MyWebServer'},
+            ]
+        }
+    ])
+instance_id = new_instances[0].id
+print(instance_id)
+new_instances[0].wait_until_running()
+print("Instance Running.")
 
-bucket_name = f"{datetime.now().strftime('%Y-%m-%d-%s')}-glipceanu"
+# s3 = boto3.resource("s3")
+# s3_client = boto3.client("s3")
+
+# bucket_name = f"{datetime.now().strftime('%Y-%m-%d-%s')}-glipceanu"
+# try:
+#     response = s3.create_bucket(Bucket=bucket_name)
+#     print (response)
+
+#     s3_client.delete_public_access_block(Bucket=bucket_name)
+
+#     website_configuration = {
+#         'ErrorDocument': {'Key': 'error.html'},
+#         'IndexDocument': {'Suffix': 'index.html'},
+#     }
+#     bucket_website = s3.BucketWebsite(bucket_name)
+#     response = bucket_website.put(WebsiteConfiguration=website_configuration)
+#     print(response)
+
+#     bucket_policy = {
+#         "Version": "2012-10-17",
+#         "Statement": [
+#             {
+#                 "Sid": "PublicReadGetObject",
+#                 "Effect": "Allow",
+#                 "Principal": "*",
+#                 "Action": "s3:GetObject",
+#                 "Resource": f"arn:aws:s3:::{bucket_name}/*"
+#             }
+#         ]
+#     }
+
+#     s3.Bucket(bucket_name).upload_file('logo.jpg', 'logo.jpg', ExtraArgs={'ContentType': 'image/jpeg'})
+
+#     bucket_policy_json = json.dumps(bucket_policy) # convert the policy to JSON and apply it
+#     s3_client.put_bucket_policy(Bucket=bucket_name, Policy=bucket_policy_json)
+
+#     print("check it")
+# except Exception as error:
+#     print (error)
+
+# MONITORING INSTALL
+print("Installing monitoring...")
+#instance_ip = new_instances[0].public_ip_address
+response = ec2_client.describe_instances(InstanceIds=[instance_id])
+instance_ip = response['Reservations'][0]['Instances'][0].get('PublicIpAddress')
 try:
-    response = s3.create_bucket(Bucket=bucket_name)
-    print (response)
-
-    s3_client.delete_public_access_block(Bucket=bucket_name)
-
-    website_configuration = {
-        'ErrorDocument': {'Key': 'error.html'},
-        'IndexDocument': {'Suffix': 'index.html'},
-    }
-    bucket_website = s3.BucketWebsite(bucket_name)
-    response = bucket_website.put(WebsiteConfiguration=website_configuration)
-    print(response)
-
-    bucket_policy = {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Sid": "PublicReadGetObject",
-                "Effect": "Allow",
-                "Principal": "*",
-                "Action": "s3:GetObject",
-                "Resource": f"arn:aws:s3:::{bucket_name}/*"
-            }
-        ]
-    }
-
-    s3.Bucket(bucket_name).upload_file('logo.jpg', 'logo.jpg', ExtraArgs={'ContentType': 'image/jpeg'})
-
-    bucket_policy_json = json.dumps(bucket_policy) # convert the policy to JSON and apply it
-    s3_client.put_bucket_policy(Bucket=bucket_name, Policy=bucket_policy_json)
-
-    print("check it")
+    print(f"Copying monitoring.sh to {instance_id} at {instance_ip}...")
+    subprocess.run(["scp", "-i", f"{keypair}.pem", "monitoring.sh", f"ec2-user@{instance_ip}:."], check=True)
+    print(f"Connecting to {instance_id} at {instance_ip}...")
+    subprocess.run(["ssh", "-i", f"{keypair}.pem", f"ec2-user@{instance_ip}", "chmod 700 monitoring.sh"], check=True)
+    print(f"Running...")
+    subprocess.run(["ssh", "-i", f"{keypair}.pem", f"ec2-user@{instance_ip}", "./monitoring.sh"], check=True)
+    print(f"Monitoring installed and running!")
 except Exception as error:
-    print (error)
+    print("Failed to install monitoring.")
+    logging.error(error) 
