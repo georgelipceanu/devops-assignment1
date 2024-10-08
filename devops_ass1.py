ac6fdea9842d8adb8d6c2de3283440b3b62b227f
georgelipceanu <george.lipceanu@gmail.com>
2024-10-09 00:02:57 +0100

    ec2 dns retrieval added

--- devops_ass1.py
+++ devops_ass1.py
@@ -1,4 +1,3 @@
-import logging.config
 import boto3
 from datetime import datetime
 import json
@@ -48,6 +47,7 @@ subprocess.run(["sudo", "chmod", "700", f"{keypair}.pem"], check=True)
 # SPINNING UP INSTANCE
 ec2 = boto3.resource('ec2')
 ec2_client = boto3.client('ec2')
+print("Starting instance...")
 new_instances = ec2.create_instances(
     ImageId=instance_image_id,
     MinCount=1,
@@ -65,14 +65,15 @@ new_instances = ec2.create_instances(
         }
     ])
 instance_id = new_instances[0].id
-print(instance_id)
+print(f"Instance ID: {instance_id}")
 new_instances[0].wait_until_running()
-print("Instance Running.")
+print("Instance Running!")
 
+# SPINNING UP BUCKET
 # s3 = boto3.resource("s3")
 # s3_client = boto3.client("s3")
-
 # bucket_name = f"{datetime.now().strftime('%Y-%m-%d-%s')}-glipceanu"
+# print(f"Launching {bucket_name}...")
 # try:
 #     response = s3.create_bucket(Bucket=bucket_name)
 #     print (response)
@@ -85,7 +86,6 @@ print("Instance Running.")
 #     }
 #     bucket_website = s3.BucketWebsite(bucket_name)
 #     response = bucket_website.put(WebsiteConfiguration=website_configuration)
-#     print(response)
 
 #     bucket_policy = {
 #         "Version": "2012-10-17",
@@ -104,23 +104,41 @@ print("Instance Running.")
 
 #     bucket_policy_json = json.dumps(bucket_policy) # convert the policy to JSON and apply it
 #     s3_client.put_bucket_policy(Bucket=bucket_name, Policy=bucket_policy_json)
-
-#     print("check it")
+#     print("Bucket Completed!")
 # except Exception as error:
 #     print (error)
+
+# WRITING URLS TO FILE
+print(f"Retrieving DNS of {instance_id}...") # INSTANCE RETRIEVAL
+try:
+    ec2_details = ec2_client.describe_instances(InstanceIds=[instance_id])
+    instance_url = ec2_details['Reservations'][0]['Instances'][0].get('PublicDnsName')
+    print("DNS retrieved!")
+except Exception as error:
+    print("Failed to retrieve instance URL")
+    logging.error(error)
+# print(f"Retrieving URL of {bucket_name}...") # BUCKET RETRIEVAL
+# try:
+#     bucket_location = s3_client.get_bucket_location(Bucket=bucket_name)
+#     region = bucket_location['LocationConstraint'] if bucket_location['LocationConstraint'] else 'us-east-1'
+# except Exception as error:
+#     print("Failed to retrieve bucket URL")
+#     logging.error(error)
+with open('glipceanu-websites.txt', 'w') as file: # WRITING TO glipceanu-websites.txt
+    file.write(instance_url)
+    #file.write(bucket_url)
 
 # MONITORING INSTALL
 print("Installing monitoring...")
 #instance_ip = new_instances[0].public_ip_address
-response = ec2_client.describe_instances(InstanceIds=[instance_id])
-instance_ip = response['Reservations'][0]['Instances'][0].get('PublicIpAddress')
+instance_ip = ec2_details['Reservations'][0]['Instances'][0].get('PublicIpAddress')
 try:
     print(f"Copying monitoring.sh to {instance_id} at {instance_ip}...")
-    subprocess.run(["scp", "-i", f"{keypair}.pem", "monitoring.sh", f"ec2-user@{instance_ip}:."], check=True)
+    subprocess.run(["scp", "-i", f"{keypair}.pem", "-o", "StrictHostKeyChecking=no", "monitoring.sh", f"ec2-user@{instance_ip}:."], check=True)
     print(f"Connecting to {instance_id} at {instance_ip}...")
-    subprocess.run(["ssh", "-i", f"{keypair}.pem", f"ec2-user@{instance_ip}", "chmod 700 monitoring.sh"], check=True)
+    subprocess.run(["ssh", "-i", f"{keypair}.pem", f"ec2-user@{instance_ip}", "-o", "StrictHostKeyChecking=no", "chmod 700 monitoring.sh"], check=True)
     print(f"Running...")
-    subprocess.run(["ssh", "-i", f"{keypair}.pem", f"ec2-user@{instance_ip}", "./monitoring.sh"], check=True)
+    subprocess.run(["ssh", "-i", f"{keypair}.pem", f"ec2-user@{instance_ip}", "-o", "StrictHostKeyChecking=no", "./monitoring.sh"], check=True)
     print(f"Monitoring installed and running!")
 except Exception as error:
     print("Failed to install monitoring.")
